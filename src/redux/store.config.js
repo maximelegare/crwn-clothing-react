0afa6d9929e29ce8c3a662752330b75266c355dc
Maxime Legare <mleg1234@protonmail.com>
2021-06-12 16:10:01 -0400

    added firebase, removed middlewares from build when app in production

--- src/redux/store.config.js
+++ src/redux/store.config.js
@@ -6,7 +6,11 @@ import rootReducer from './root-reducer'
 import logger from 'redux-logger'
 // import { persistStore } from 'redux-persist'
 
-const middleWares = [logger]
+const middleWares = []
+
+if(process.env.NODE_ENV === 'development'){
+    middleWares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares))
 
